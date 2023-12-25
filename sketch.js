74e55c9db590d1692fdc00beec6bea70205e9dcd
nini0924 <154111227+nini0924@users.noreply.github.com>
2023-12-25 11:04:51 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://nini0924.github.io/kk2002/model.json';
 
 
 function preload() {
